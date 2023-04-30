6b1b3c362032866cabf6c5a385decc4b170d44af
Vaibhav Pawar <102847008+vp-1234ms@users.noreply.github.com>
2023-04-30 14:38:35 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, app,render_template
+from flask import Response
+
+
+application = Flask(__name__)
+app=application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
